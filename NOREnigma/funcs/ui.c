b23695b896f146071181788fcafbe41c03b8bfbe
Virna Amaral <vfpa@cesar.school>
2023-11-11 20:43:12 -0300

    adicionando colaborador no sobre

--- NOREnigma/funcs/ui.c
+++ NOREnigma/funcs/ui.c
@@ -58,5 +58,5 @@ void menu_inicial(){
 }
 
 void sobre(){
-    printf("Desenvolvido em 2023, NOREnigma eh um projeto para a cadeira de Algoritmos e Estrutura de Dados do terceiro periodo. Tinhamos a tarefa de criar um game totalmente do zero em C, tendo como enfoque alguma estrutura de dados de nossa escolha que foi abordada durante o periodo. Escolhemos utilizar listas duplamente encadeadas pela versatilidade e facilidade na manipulacao dos dados com ela.\n\nDEVS:\nPedro Monteiro\nVirna Amaral");
+    printf("Desenvolvido em 2023, NOREnigma eh um projeto para a cadeira de Algoritmos e Estrutura de Dados do terceiro periodo. Tinhamos a tarefa de criar um game totalmente do zero em C, tendo como enfoque alguma estrutura de dados de nossa escolha que foi abordada durante o periodo. Escolhemos utilizar listas duplamente encadeadas pela versatilidade e facilidade na manipulacao dos dados com ela.\n\nTIME:\nPedro Monteiro\nVirna Amaral\nEduarda Aguiar\n");
 }
