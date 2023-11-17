9ac259b7b333b3e2cb7fbbed5e60a20df1ba7a78
pdrvsk <nouoneq@riseup.net>
2023-11-17 09:21:38 -0300

    Makefile criado e funcionalidade p/ pular o texto da história p/ o final quando aperta qualquer tecla

--- NOREnigma/funcs/ui.c
+++ NOREnigma/funcs/ui.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
-#include <conio.h>
 
 #ifdef _WIN32
     #include <windows.h>
-    
+    #include <conio.h>
 #else
+    #include <termios.h>
+    #include <fcntl.h>
     #include <unistd.h>
+    #include <curses.h>
 #endif
 
 void setUtf8Encoding() {
@@ -40,17 +42,78 @@ void pausa() {
     getchar();
 }
 
+int kbhit(void){
+  struct termios oldt, new_t;
+  int ch;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+
+  new_t = oldt;
+  new_t.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &new_t);
+
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if(ch != EOF){
+    ungetc(ch, stdin);
+    return 1;
+  }
+  return 0;
+
+}
+
+
 void imprimir_com_pausa(char *mensagem, int pausa_ms) {
     for (int i = 0; mensagem[i] != '\0'; i++) {
         fputc(mensagem[i], stdout);
         fflush(stdout);
 
-        if (kbhit()) {
-            fflush(stdin);
-            printf("%s", &mensagem[i+1]);
+        #ifdef _WIN32
+            if (kbhit()) {
+                fflush(stdin);
+                printf("%s", &mensagem[i+1]);
             break;
         }
-        
+
+        #else
+            if(kbhit()){
+	      while(kbhit()){
+		getchar();
+	      }
+	      printf("%s", &mensagem[i+1]);
+	      break;
+	    }
+	    //usleep(pausa_ms * 1000);
+
+
+        #endif
+	// Se for windows, rodar o if abaixo
+	    //	if(kbhit()){
+	    //	  while(kbhit()){
+	    //	    getchar();
+	    //	  }
+	    //	  printf("%s", mensagem[i+1]);
+	    //	  break;
+	    //	}
+	    //	usleep(pausa_ms * 1000);
+
+
+	//	
+	  //	// Se for windows, rodar esse
+	  //        if (kbhit()) {
+	  //            fflush(stdin);
+	  //            printf("%s", &mensagem[i+1]);
+	  //            break;
+	  //        }
+	//
+	
         #ifdef _WIN32
             Sleep(pausa_ms);
         #else
